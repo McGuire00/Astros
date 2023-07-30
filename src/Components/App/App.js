bbe29431fde7df89344c64acb8d5ab2d0efcec20
Deron <deronmcguire23@yahoo.com>
2023-07-30 15:21:02 -0500

    Get pitches data and set it as state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,28 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+
+import Astros from "../../util/Astros";
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    getBaseballData();
+  }, []);
+
+  function getBaseballData() {
+    Astros.getPitches().then((data) => {
+      setData(data);
+    });
+  }
   return (
     <div className="App">
-     <Header/>
-     <Main/>
-     <Footer/>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
