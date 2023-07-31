c1bd0cef96671f44c2a7fa88cded9ee9153c3553
Deron <deronmcguire23@yahoo.com>
2023-07-31 17:55:05 -0500

    Built out header component with styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,16 @@
-import './Header.css';
+import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-     Hello Header
+      <h1 className="header-title">Baseball Data</h1>
+      <nav className="header-nav">
+        <ul className="header-nav-list">
+          <li className="header-nav-item">Players</li>
+          <li className="header-nav-item">Games</li>
+          <li className="header-nav-item">Contact</li>
+        </ul>
+      </nav>
     </div>
   );
 }
