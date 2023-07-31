c0ab6c775259e32d9a6c173a21fea58265023ba0
Deron <deronmcguire23@yahoo.com>
2023-07-31 17:47:11 -0500

    Changed function names that gets baseball data and correct css styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,14 +6,26 @@ import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 
 import Astros from "../../util/Astros";
+import PitcherList from "../PitcherList/PitcherList";
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <PitcherList />
+//     </div>
+//   );
+// };
+//
+// export default App;
+
 function App() {
   const [data, setData] = useState();
 
   useEffect(() => {
-    getBaseballData();
+    getPitchData();
   }, []);
 
-  function getBaseballData() {
+  function getPitchData() {
     Astros.getPitches().then((data) => {
       setData(data);
     });
