8b489d5f0fb1fbd2f8e94b34b7f02b5d551c86b3
Deron <deronmcguire23@yahoo.com>
2023-08-01 10:06:51 -0500

    Changed class name to match actual data not mock

--- src/Components/Pitcher/Pitcher.js
+++ src/Components/Pitcher/Pitcher.js
@@ -6,9 +6,7 @@ function Pitcher({ player }) {
         <div className="player-name">{player.name}</div>
         <div className="player-info">
           <span className="player-team">Team: {player.team}</span>
-          <span className="player-average">
-            Batting Avg: {player.battingAverage}
-          </span>
+          <span className="player-throw">Throws: {player.throws}</span>
         </div>
       </li>
     </div>
