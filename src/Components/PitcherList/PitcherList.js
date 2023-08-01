4cc52d502d257521048e1f59d7658599f656c600
Deron <deronmcguire23@yahoo.com>
2023-08-01 10:08:34 -0500

    Deleted mock data and used actual data and set fixed height for container

--- src/Components/PitcherList/PitcherList.js
+++ src/Components/PitcherList/PitcherList.js
@@ -2,25 +2,10 @@ import { useState, useEffect } from "react";
 import "./PitcherList.css";
 
 import Pitcher from "../Pitcher/Pitcher";
-function PitcherList() {
-  // Mock data for MLB players and their stats
-  const mockData = [
-    { id: 1, name: "Player A", team: "Team X", battingAverage: 0.302 },
-    { id: 2, name: "Player B", team: "Team Y", battingAverage: 0.275 },
-    { id: 3, name: "Player C", team: "Team Z", battingAverage: 0.32 },
-    { id: 1, name: "Player A", team: "Team X", battingAverage: 0.302 },
-    { id: 2, name: "Player B", team: "Team Y", battingAverage: 0.275 },
-    { id: 3, name: "Player C", team: "Team Z", battingAverage: 0.32 },
-    { id: 1, name: "Player A", team: "Team X", battingAverage: 0.302 },
-    { id: 2, name: "Player B", team: "Team Y", battingAverage: 0.275 },
-    { id: 3, name: "Player C", team: "Team Z", battingAverage: 0.32 },
-    // Add more players here...
-  ];
-
-  const [players, setPlayers] = useState(mockData);
+function PitcherList({ players }) {
   return (
     <div className="container">
-      <h1 className="players-title">MLB Players Stats</h1>
+      <h1 className="players-title">Players</h1>
       <ul className="players-list">
         {players.map((player) => (
           <Pitcher key={player.id} player={player} />
