8e64c8e3e514ff5a2bc034452b8941284353d800
Deron <deronmcguire23@yahoo.com>
2023-08-01 10:13:02 -0500

    Added method to get unique pitchers

--- src/util/Astros.js
+++ src/util/Astros.js
@@ -10,6 +10,24 @@ const Astros = {
         return jsonResponse.queryResults.row;
       });
   },
+  getUniquePitchers(pitchData) {
+    let pitchers = [];
+
+    for (let i = 0; i < pitchData.length; i++) {
+      let pitcher = pitchData[i];
+
+      if (!pitchers.map((player) => player.id).includes(pitcher.pitcher_id)) {
+        pitchers.push({
+          id: pitcher.pitcher_id,
+          name: pitcher.pitcher_name,
+          throws: pitcher.pitcher_throws,
+          team: pitcher.fielding_team_name,
+          team_code: pitcher.fielding_team_code,
+        });
+      }
+    }
+    return pitchers;
+  },
 };
 
 export default Astros;
